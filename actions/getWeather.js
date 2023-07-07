45d2fa4044f6c2c4dd270a3ee566a02d3a3f2916
Santi Teran <santiagoteranmatias@gmail.com>
2023-07-07 20:12:32 -0300

    commit

--- actions/getWeather.js
+++ actions/getWeather.js
@@ -1,5 +1,5 @@
 export default async function getWeather(location) {
-  const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=7e27afc0701647c3b0a144233230407&q=${location}&days=8&aqi=yes&alerts=yes`);
+  const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=7e27afc0701647c3b0a144233230407&q=${location}&days=8&aqi=yes&alerts=yes`);
   const data = await response.json();
   return data;
 }
